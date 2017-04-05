408214a5291b54b4470ed2ff1a55f2d5a1ed9dc3
Unknown <ze_yaya@msn.com>
2017-04-05 13:38:11 +1000

    removed file

--- functions/emailer.php
+++ functions/emailer.php
@@ -9,7 +9,7 @@ function Emailer($toEmail, $message, $Mailsubject, $fromEmail, $fromName ,$sendi
 	if ($fromEmail == null) $fromEmail = $sendingEmail;
 	if ($sendingName == null) $sendingName = "PTR Management";
 	if ($fromName == null) $fromName = "PTR Management";
-	$apiKey = 'please see file(right click open with notepad)';
+	$apiKey = 'please see mailkey in google drive';
 	$sg = new SendGrid($apiKey);
 	$data = ['personalizations' => [['to' => [['email' => $toEmail]], 'subject' => $Mailsubject]], 'from' => ['email' => $sendingEmail, 'name' => $sendingName ], 'reply_to' => ['email' => $fromEmail, 'name' => $fromName], 'content' => [['type' => 'text', 'value' => $message]]];
 	if (!empty($filePath) && !empty($fileNametoDisplay))
